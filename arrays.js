5a1a0ef75a882b4bf822b79f91dfda2e36f37e7b
Ali Pfeffer <alpfefs@gmail.com>
2019-07-24 17:31:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,6 @@ function addElementToBeginningOfArray(array,element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
- array[1] = element
+ array[0] = element
  return array
 }

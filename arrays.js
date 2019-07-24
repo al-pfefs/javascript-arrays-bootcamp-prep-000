9a8a2304223c0b2a1e086c7714e5ae88eb9eb498
Ali Pfeffer <alpfefs@gmail.com>
2019-07-24 17:02:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToEndOfArray(array,element) {
-  
+  array
 }
 
 function destructivelyAddElementToEndOfArray(array,element) {

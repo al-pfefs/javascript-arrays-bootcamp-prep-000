63e74ec30d3f8a7d2caee5cd51ef9869ded2ac6d
Ali Pfeffer <alpfefs@gmail.com>
2019-07-24 17:12:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array,element) {
   array[1] = element
+  array
 }
 
 function destructivelyAddElementToEndOfArray(array,element) {
